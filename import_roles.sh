f548743ecce2f5ba1004ad229bb8dfb45a079516
brice <maxpayne911@hotmail.com>
2017-01-04 15:15:04 +0100

    added local nfs mount to "bypass" nginx chroot

--- import_roles.sh
+++ import_roles.sh
@@ -10,12 +10,18 @@ git_url_matching="^((git|ssh|http(s)?)|(git@[\w\.]+))(:(//)?)"
 if [ ! -d $roles_dir ]; then
   mkdir $roles_dir
 fi
- 
+
+echo "git cloning roles..."
 for role in "${roles_list[@]}"
 do 
-#  echo $role
   if [[ "$role" =~ $git_url_matching ]]; then
-    echo "cloning role : $role ..."
     cd "$roles_dir" && git clone $role 
   fi 
+done
+
+echo "git update..."
+cd "$roles_dir"
+for dir in `ls $roles_dir`
+do
+  echo -n "$dir : " &&  cd $roles_dir/$dir && git pull
 done
